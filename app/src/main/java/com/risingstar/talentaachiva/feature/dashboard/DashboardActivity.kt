29c530ea56370416b18d3fff363ace13d068a591
Haios-Paradox <haiosvida@gmail.com>
2022-06-02 22:07:08 +0700

    Implemented Navigation Drawer

--- app/src/main/java/com/risingstar/talentaachiva/feature/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/risingstar/talentaachiva/feature/dashboard/DashboardActivity.kt
@@ -2,23 +2,35 @@ package com.risingstar.talentaachiva.feature.dashboard
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import com.risingstar.talentaachiva.R
 import com.risingstar.talentaachiva.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding : ActivityDashboardBinding
     private lateinit var viewmodel : DebugVM
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val navHostFragment =
-//            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-//        val navController = navHostFragment.navController
-//        findViewById<NavigationView>(R.id.nav_view)
-//            .setupWithNavController(navController)
-//
-//        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
 
+        setSupportActionBar(binding.appBarDashboardActivity.toolbar)
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+
+        val navController =
+            findNavController(R.id.nav_host_fragment_content_dashboard_activity)
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
 
         binding = ActivityDashboardBinding.inflate(layoutInflater)
