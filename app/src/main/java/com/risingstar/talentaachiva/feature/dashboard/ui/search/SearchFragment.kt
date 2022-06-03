d6ac9829d023c4b952612bf0dbd781a27271c164
Haios-Paradox <haiosvida@gmail.com>
2022-06-03 21:44:02 +0700

    Colors of Fishes in the Sky

--- app/src/main/java/com/risingstar/talentaachiva/feature/dashboard/ui/search/SearchFragment.kt
+++ app/src/main/java/com/risingstar/talentaachiva/feature/dashboard/ui/search/SearchFragment.kt
@@ -5,16 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.risingstar.talentaachiva.R
+import com.risingstar.talentaachiva.databinding.FragmentSearchBinding
 
 
 class SearchFragment : Fragment() {
-
+    private lateinit var binding : FragmentSearchBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        binding = FragmentSearchBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 }
