6d127cdff1da2dea9fdc673ab6100b87f99b350e
nieelz <nieelz.id@gmail.com>
2022-06-11 14:39:21 +0700

    fixing settings fragment, but no logout feature

--- app/src/main/java/com/risingstar/talentaachiva/feature/dashboard/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/risingstar/talentaachiva/feature/dashboard/ui/settings/SettingsFragment.kt
@@ -1,25 +1,47 @@
 package com.risingstar.talentaachiva.feature.dashboard.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.risingstar.talentaachiva.R
-import com.risingstar.talentaachiva.databinding.FragmentSearchBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.risingstar.talentaachiva.databinding.FragmentSettingsBinding
+import com.risingstar.talentaachiva.feature.dashboard.DashboardVM
+import com.risingstar.talentaachiva.feature.organizer.OrganizerActivity
 
 class SettingsFragment : Fragment() {
-    private lateinit var binding : FragmentSettingsBinding
+    private var _binding: FragmentSettingsBinding? = null
+    private lateinit var viewmodel: DashboardVM
+
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentSettingsBinding.inflate(layoutInflater,container,false)
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+
+
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        viewmodel = ViewModelProvider(requireActivity()).get(DashboardVM::class.java)
+
+        binding.buttonSwitchOrg.setOnClickListener{
+            val intent = Intent(requireActivity(), OrganizerActivity::class.java)
+            intent.putExtra(OrganizerActivity.CURRENT_USER_ID,viewmodel.userID)
+            startActivity(intent)
+        }
+
+        binding.buttonLogout.setOnClickListener{
+
+        }
+
+
         return binding.root
+
+
     }
 
 
